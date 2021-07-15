ab57c26fd1bfb90382f5bb2f62d44fcc2895b29e
Sarfaraz <m.s.hussain@outlook.com>
2021-07-15 13:00:52 -0500

    created get route for /api/notes

--- Develop/server.js
+++ Develop/server.js
@@ -10,6 +10,11 @@ app.use(express.static("public"))
 app.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 }) 
+app.get("/api/notes", function(req, res){
+    const notes = fs.readFileSync(path.join(__dirname, "./db/db.json" ), "utf8")
+    console.log(notes)
+    res.json(JSON.parse(notes))
+})
 
 
 

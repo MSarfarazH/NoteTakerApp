20bce4237ff658dae4c1cdd84ab5b3142ced4feb
Sarfaraz <m.s.hussain@outlook.com>
2021-07-15 12:59:22 -0500

    created get route for /notes

--- Develop/server.js
+++ Develop/server.js
@@ -7,6 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.static("public"))
 
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+}) 
+
 
 
 
